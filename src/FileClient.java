b61262d2a45549950685c450d53f4038a863f306
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-09-17 22:14:44 -0500

    Update

--- src/FileClient.java
+++ src/FileClient.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 class FileClient {
@@ -19,27 +16,22 @@ class FileClient {
         System.out.println("Attempting to connect to host " +
                 serverHostname + " on port " + SERVER_PORT + ".");
 
+        File file = new File(fileToUpload);
+
         // Initialize resource dependent objects as parameters within the try block.
         // All objects will automatically close at the end of the try block.
         // The serverSocket is created to connect to server.
         // Once the client has connected, the "out" object is declared to send file data
         // to the server. A file reader is also created.
         try (Socket serverSocket = new Socket(serverHostname, SERVER_PORT);
-             PrintWriter out = new PrintWriter(
-                     serverSocket.getOutputStream(), true);
-             BufferedReader fileIn = new BufferedReader(
-                     new FileReader(fileToUpload))
+             OutputStream outputStream = serverSocket.getOutputStream();
+             BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))
         ) {
-            // If the past connections and object creation are successful, then we
-            // are able to proceed in uploading the file data to the server.
-            System.out.println("Uploading " + fileToUpload + " to server.");
-
-            // The client will continue sending data to the server until it reaches end of file.
-            for (String line = fileIn.readLine(); line != null; line = fileIn.readLine())
-                out.println(line);
-
-            // The upload is complete at this point.
-            System.out.println("Upload Complete.");
+            System.out.println("Connection successful.\nUploading file...");
+            byte[] data = new byte[(int) file.length()]; // Initialize the size of the byte array.
+            inputStream.read(data,0, data.length); // Read the file data into array.
+            outputStream.write(data,0, data.length); // Stream data to server.
+            System.out.println("File upload complete.");
         } catch (IOException e) {
             // print detailed error stack trace.
             e.printStackTrace();
